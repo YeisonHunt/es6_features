8aa3022e4558873daace316a0511479c76cc3564
YeisonHunt <yecaicedo@unicauca.edu.co>
2021-10-29 16:31:19 -0500

    Configured repository for es6 and deno

---
+++ let_cons.ts
@@ -0,0 +1,17 @@
+// In the past we used the var keyword for every variable. ES5
+
+//For example 
+
+var name = 'jade'; //Bad practice today!
+name = 'bob';
+
+console.log(name);
+
+// Now for ES6, we can use the let keyboard, which doesn't have the global scope 
+// to avoid possible re assignations or unwanted assigns.
+
+let anotherName = 'yeison';
+anotherName = 'otherYeison';
+
+console.log(anotherName)
+
